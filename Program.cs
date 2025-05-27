d25b1ee878f1fdc536da2c155c0453f19c2e5a12
vanessa <vanessa.f.ashes@gmail.com>
2025-05-27 12:14:44 +0200

    Delete Program.cs

--- Program.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AFamiliarWorld
-{
-    public class Program
-    {
-        public static void Main(string[] args) =>
-            new AFamiliarWorld.Bot.AFamiliarWorld().RunAsync(args[0]).GetAwaiter().GetResult();
-    }
-    
-}
