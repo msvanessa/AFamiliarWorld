b0b8d0602fca8b3ea4a3ee8aa3921d4f2adcc12b
Zoey <40408923+Noele@users.noreply.github.com>
2025-05-28 01:35:05 +0100

    refactor:FamiliarBattles Remove duplicated code
    The code inside of FamiliarBattles is duplicated for each attacker, create a function for each of the duplicated actions, and call them for each

--- AFamiliarWorld/Bot/Commands/FamiliarBattles.cs
+++ AFamiliarWorld/Bot/Commands/FamiliarBattles.cs
@@ -1,7 +1,9 @@
 using AFamiliarWorld.Bot.Commands.Models;
+using AFamiliarWorld.Bot.Familiars;
 using Discord;
 using Discord.Addons.Interactive;
 using Discord.Commands;
+using Discord.Rest;
 using Discord.WebSocket;
 using Newtonsoft.Json;
 
@@ -87,121 +89,23 @@ public class FamiliarBattles
 
                 while (firstAttacker.Health >= 0 && secondAttacker.Health >= 0)
                 {
-                    var firstAttackerStatusConditions = await firstAttacker.GetStatusConditions();
-                    if (!firstAttackerStatusConditions.Contains(StatusCondition.Stun))
-                    {
-                            
-                        
-                        var attack = await firstAttacker.Attack();
-                        var defend = await secondAttacker.Defend(attack);
-                        secondAttacker.Health -= defend;
-                        var criticalHit = attack.CriticalHit == true ? "***" : "";
-                        
-                        embed.WithFields().AddField($"{criticalHit}{firstAttackerUser.Username}'s {firstAttacker.Name} attacks {secondAttackerUser.Username}'s {secondAttacker.Name} with {attack.AbilityName} for {defend} damage!{criticalHit}", $"{secondAttackerUser.Username}'s {secondAttacker.Name} has {secondAttacker.Health} health remaining.");
-                        await reply.ModifyAsync(new Action<MessageProperties>(props =>
-                        {
-                            props.Embed = embed.Build();
-                        }));
-       
-                        if (secondAttacker.Health <= 0)
-                        {
-                            var victorEmbed = new EmbedBuilder();
-                            victorEmbed.WithColor(Discord.Color.Gold);
-                            victorEmbed.WithTitle($"{firstAttackerUser.Username}'s {firstAttacker.Name} wins!");
-                            victorEmbed.WithImageUrl(
-                                "https://cdn.discordapp.com/attachments/803309924746395691/1376829532660568064/victory-pop-up-golden-assets-award-with-crown-for-game-illustration-golden-banner-with-wings-and-red-flags-vector.jpg?ex=6836bfec&is=68356e6c&hm=ffe35dc277c4b053ae396cf905581e05767837143ef2fffa6305203be11c08e7&");
-                            victorEmbed.WithThumbnailUrl(firstAttackerUser.GetAvatarUrl());
-                            await ReplyAsync(embed: victorEmbed.Build());
-                            break;
-                        }
-                    }
-                    else
-                    {
-                        embed.WithFields().AddField("Stunned!", $"{firstAttackerUser.Username}'s {firstAttacker.Name} is stunned and cannot attack this turn.");
-                        await firstAttacker.RemoveStatusCondition(StatusCondition.Stun);
-                    }
+                    var firstAttackerIsWinner = await DoTurn(firstAttacker, secondAttacker, firstAttackerUser, secondAttackerUser, embed, reply, Context);
+                    if (firstAttackerIsWinner) break;
+                    
                     await Task.Delay(3000);
                     
-                    var secondAttackerStatusConditions = await secondAttacker.GetStatusConditions();
-                    if (!secondAttackerStatusConditions.Contains(StatusCondition.Stun))
-                    {
-                        var attack = await secondAttacker.Attack();
-                        var defend = await firstAttacker.Defend(attack);
-                        firstAttacker.Health -= defend;
-                        var criticalHit = attack.CriticalHit == true ? "**" : "";
-
-                        embed.WithFields().AddField(
-                            $"{criticalHit}{secondAttackerUser.Username}'s {secondAttacker.Name} attacks {firstAttackerUser.Username}'s {firstAttacker.Name} with {attack.AbilityName} for {defend} damage!{criticalHit}",
-                            $"{firstAttackerUser.Username}'s {firstAttacker.Name} has {firstAttacker.Health} health remaining.");
-                        await reply.ModifyAsync(
-                            new Action<MessageProperties>(props => { props.Embed = embed.Build(); }));
-
-                        if (firstAttacker.Health <= 0)
-                        {
-                            var victorEmbed = new EmbedBuilder();
-                            victorEmbed.WithColor(Discord.Color.Gold);
-                            victorEmbed.WithTitle($"{secondAttackerUser.Username}'s {secondAttacker.Name} wins!");
-                            victorEmbed.WithImageUrl(
-                                "https://cdn.discordapp.com/attachments/803309924746395691/1376829532660568064/victory-pop-up-golden-assets-award-with-crown-for-game-illustration-golden-banner-with-wings-and-red-flags-vector.jpg?ex=6836bfec&is=68356e6c&hm=ffe35dc277c4b053ae396cf905581e05767837143ef2fffa6305203be11c08e7&");
-                            victorEmbed.WithThumbnailUrl(secondAttackerUser.GetAvatarUrl());
-                            await ReplyAsync(embed: victorEmbed.Build());
-
-                            break;
-                        }
-                    }
-                    else
-                    {
-                        embed.WithFields().AddField("Stunned!", $"{secondAttackerUser.Username}'s {secondAttacker.Name} is stunned and cannot attack this turn.");
-                        await secondAttacker.RemoveStatusCondition(StatusCondition.Stun);
-                    }
-
-                    foreach (var statuscondition in await firstAttacker.GetStatusConditions())
-                    {
-                        switch (statuscondition)
-                        {
-                            case StatusCondition.Burn:
-                                firstAttacker.Health -= 2;
-                                embed.WithFields().AddField($"{firstAttackerUser.Username}'s {firstAttacker.Name} is burning! They take 2 fire damage.", $"{firstAttackerUser.Username}'s {firstAttacker.Name} has {firstAttacker.Health} health remaining.");
-                                break;
-                            case StatusCondition.None:
-                                break;
-                        }
-                    }
-                    foreach (var statuscondition in await secondAttacker.GetStatusConditions())
-                    {
-                        switch (statuscondition)
-                        {
-                            case StatusCondition.Burn:
-                                secondAttacker.Health -= 2;
-                                embed.WithFields().AddField($"{secondAttackerUser.Username}'s {secondAttacker.Name} is burning! They take 2 fire damage.", $"{secondAttackerUser.Username}'s {secondAttacker.Name} has {secondAttacker.Health} health remaining.");
-                                break;
-                            case StatusCondition.None:
-                                break;
-                        }
-                    }
-                    if (secondAttacker.Health <= 0)
-                    {
-                        var victorEmbed = new EmbedBuilder();
-                        victorEmbed.WithColor(Discord.Color.Gold);
-                        victorEmbed.WithTitle($"{firstAttackerUser.Username}'s {firstAttacker.Name} wins!");
-                        victorEmbed.WithImageUrl(
-                            "https://cdn.discordapp.com/attachments/803309924746395691/1376829532660568064/victory-pop-up-golden-assets-award-with-crown-for-game-illustration-golden-banner-with-wings-and-red-flags-vector.jpg?ex=6836bfec&is=68356e6c&hm=ffe35dc277c4b053ae396cf905581e05767837143ef2fffa6305203be11c08e7&");
-                        victorEmbed.WithThumbnailUrl(firstAttackerUser.GetAvatarUrl());
-                        await ReplyAsync(embed: victorEmbed.Build());
-                        break;
-                    }
-                    if (firstAttacker.Health <= 0)
-                    {
-                        var victorEmbed = new EmbedBuilder();
-                        victorEmbed.WithColor(Discord.Color.Gold);
-                        victorEmbed.WithTitle($"{secondAttackerUser.Username}'s {secondAttacker.Name} wins!");
-                        victorEmbed.WithImageUrl(
-                            "https://cdn.discordapp.com/attachments/803309924746395691/1376829532660568064/victory-pop-up-golden-assets-award-with-crown-for-game-illustration-golden-banner-with-wings-and-red-flags-vector.jpg?ex=6836bfec&is=68356e6c&hm=ffe35dc277c4b053ae396cf905581e05767837143ef2fffa6305203be11c08e7&");
-                        victorEmbed.WithThumbnailUrl(secondAttackerUser.GetAvatarUrl());
-                        await ReplyAsync(embed: victorEmbed.Build());
-
-                        break;
-                    }
+                    var secondAttackerIsWinner = await DoTurn(secondAttacker, firstAttacker, secondAttackerUser, firstAttackerUser, embed, reply, Context);
+                    if (secondAttackerIsWinner) break;
+                    
+                    await CheckStatusCondition(firstAttacker, firstAttackerUser, embed);
+                    await CheckStatusCondition(secondAttacker, secondAttackerUser, embed);
+                    
+                    var firstWinner = CheckWinner(Context, firstAttacker, firstAttackerUser, secondAttacker, secondAttackerUser);
+                    if (firstWinner) break;
+                    
+                    var secondWinner = CheckWinner(Context, secondAttacker, secondAttackerUser, firstAttacker, firstAttackerUser);
+                    if (secondWinner) break;
+                    
                     await Task.Delay(3000);
                 }
             }
@@ -209,6 +113,63 @@ public class FamiliarBattles
             {
                 await ReplyAsync($"An error occurred: {ex.Message}");
             }
+        }
+
+        private static async Task<bool> DoTurn(Familiar firstAttacker, Familiar secondAttacker, SocketUser firstAttackerUser, SocketUser secondAttackerUser, EmbedBuilder embed, RestUserMessage reply, SocketCommandContext context)
+        {
+            var secondAttackerStatusConditions = await secondAttacker.GetStatusConditions();
+            if (!secondAttackerStatusConditions.Contains(StatusCondition.Stun))
+            {
+                var attack = await secondAttacker.Attack();
+                var defend = await firstAttacker.Defend(attack);
+                firstAttacker.Health -= defend;
+                var criticalHit = attack.CriticalHit == true ? "***" : "";
+
+                embed.WithFields().AddField(
+                    $"{criticalHit}{secondAttackerUser.Username}'s {secondAttacker.Name} attacks {firstAttackerUser.Username}'s {firstAttacker.Name} with {attack.AbilityName} for {defend} damage!{criticalHit}",
+                    $"{firstAttackerUser.Username}'s {firstAttacker.Name} has {firstAttacker.Health} health remaining.");
+                await reply.ModifyAsync(
+                    new Action<MessageProperties>(props => { props.Embed = embed.Build(); }));
+                var winner = CheckWinner(context, secondAttacker, secondAttackerUser, firstAttacker, firstAttackerUser);
+                return winner;
+            }
+            else
+            {
+                embed.WithFields().AddField("Stunned!", $"{secondAttackerUser.Username}'s {secondAttacker.Name} is stunned and cannot attack this turn.");
+                await secondAttacker.RemoveStatusCondition(StatusCondition.Stun);
+            }
+
+            return false;
+        }
+        private static async Task CheckStatusCondition(Familiar familiar, SocketUser user, EmbedBuilder embed)
+        {
+            foreach (var statusCondition in await familiar.GetStatusConditions())
+            {
+                switch (statusCondition)
+                {
+                    case StatusCondition.Burn:
+                        familiar.Health -= 2;
+                        embed.WithFields().AddField($"{user.Username}'s {familiar.Name} is burning! They take 2 fire damage.", $"{user.Username}'s {familiar.Name} has {familiar.Health} health remaining.");
+                        break;
+                    case StatusCondition.None:
+                        break;
+                }
+            }
+        }
+        
+        private static bool CheckWinner(SocketCommandContext context, Familiar winnerFamiliar, SocketUser winnerUser, Familiar loserFamiliar, SocketUser loserUser)
+        {
+            if (loserFamiliar.Health <= 0)
+            {
+                var embed = new EmbedBuilder();
+                embed.WithColor(Discord.Color.Red);
+                embed.WithTitle($"{winnerUser.Username}'s {winnerFamiliar.Name} wins!");
+                embed.WithImageUrl("https://cdn.discordapp.com/attachments/803309924746395691/1376829532660568064/victory-pop-up-golden-assets-award-with-crown-for-game-illustration-golden-banner-with-wings-and-red-flags-vector.jpg?ex=6836bfec&is=68356e6c&hm=ffe35dc277c4b053ae396cf905581e05767837143ef2fffa6305203be11c08e7&");
+                embed.WithThumbnailUrl(winnerUser.GetAvatarUrl());
+                context.Channel.SendMessageAsync(embed: embed.Build());
+                return true;
+            }
+            return false;
         }
         
         private async Task<SocketMessage> WaitForMessageAsync(SocketUser user, IMessageChannel channel, TimeSpan timeout)
