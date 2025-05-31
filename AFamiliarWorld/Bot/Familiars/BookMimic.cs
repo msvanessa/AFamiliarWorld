a5e50c05b5cc3019744f8a0bf350bd2f3ba2b6c6
Zoey <40408923+Noele@users.noreply.github.com>
2025-05-31 13:51:38 +0100

    fix: Merge last push was malformed, merge correctly

--- AFamiliarWorld/Bot/Familiars/BookMimic.cs
+++ AFamiliarWorld/Bot/Familiars/BookMimic.cs
@@ -15,6 +15,7 @@ public class BookMimic:Familiar
         var random = new Random();
         this.Name = "BookMimic";
         this.Description = "Read me pls c:<";
+        this.Emoji = "<:FamiliarBook:1378164196457320551>";
         this.Quip = "*Bites u*";
         this.Color = 0xa7743e;
         this.Url = "https://media.discordapp.net/attachments/1246170699362729995/1375188921834668102/assets_task_01jvwpbm43fc7r95023q0ca9ax_1747940954_img_3.webp?ex=683170bc&is=68301f3c&hm=e8e705ab8ca5f2c7eddb82de4b7d355be1330eebe9627a83a34facf63ef911f2&=&format=webp&width=645&height=968";
